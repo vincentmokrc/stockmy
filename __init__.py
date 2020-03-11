5265caaf99004a804142021c81e731a968a24be0
vincentmokrc <vincentbiz@outlook.com>
2020-03-11 21:22:41 +0800

    Update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,24 @@
-from bursamy.download import BursaFileDownloader
+#from bursamy.download import BursaFileDownloader
 import time
+from bursamy.xml import XMLConvertor
+#from convert_pdf import convert_pdf
 
 start_time = time.time()
-BursaFileDownloader("0208").get_financial_result()
+#BursaFileDownloader("9385").get_financial_result()
+#dir_path = "C:\\fs\\test\\test.pdf"
+
+#LINUX convert
+#dir_path = "./test"
+#print(dir_path)
+#PdfConverter(dir_path).covert_all_to_xml()
+#xml = convert_pdf(dir_path,'xml')
+#print(xml)
+
+
+########
+dir_path = "C://Users//vince//stockmy//test//xml"
+XMLConvertor(dir_path).open_xml()
+#####
+
 print("--- %s seconds ---" % (time.time() - start_time))
 
